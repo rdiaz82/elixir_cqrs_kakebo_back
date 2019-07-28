25f5bb847964b8d20bb69e09e4223ca5a46cd3a5
Roberto Diaz <roberto@theagilemonkeys.com>
2019-07-29 00:54:00 +0100

    added main app interface

--- lib/cqrs.ex
+++ lib/cqrs.ex
@@ -1,5 +1,29 @@
 defmodule Cqrs do
-  @moduledoc """
-  Documentation for Cqrs.
-  """
+  alias Cqrs.Application
+  alias Cqrs.Router
+
+  alias Cqrs.Commands.AddMoney
+  alias Cqrs.Commands.ExpendMoney
+
+
+  def start do
+    Application.start(:cqrs, [])
+  end
+
+  def addMoneyCommand(account, amount, concept) do
+      Router.dispatch(%AddMoney{
+        account: account,
+        amount: amount,
+        concept: concept
+      })
+  end
+
+  def expendMoneyCommand(account, amount, concept, category) do
+    Router.dispatch(%ExpendMoney{
+      account: account,
+      amount: amount,
+      concept: concept,
+      category: category
+    })
+  end
 end
